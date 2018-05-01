c49e38d667792a45a746bd77c8b5209ec13dbaaf
xnick <1256623447@qq.com>
2018-05-02 00:05:49 +0800

    a problem that scrolling in find page is not solved,and all sizes have changed not comletely considering our ui

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
-var app = new Framework7();
+var app = new Framework7({
+	routes: [
+		{
+			path: '/home/',
+			url: 'index.html',
+		},
+		{
+			path: '/find/',
+			url: './pages/find.html',
+		}, 
+	],
+});
 var $$ = Dom7;
 var mainView = app.views.create('.view-main');
