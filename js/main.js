6aeffe9ca7ca296d8cc74168ff22571a2839f609
xnick <1256623447@qq.com>
2018-05-03 22:05:31 +0800

    give you task

--- js/main.js
+++ js/main.js
@@ -8,7 +8,21 @@ var app = new Framework7({
 			path: '/find/',
 			url: './pages/find.html',
 		}, 
+		{
+			path: '/myown/',
+			url:'./pages/myown.html',
+		},
 	],
 });
 var $$ = Dom7;
 var mainView = app.views.create('.view-main');
+var mySwiper = new Swiper('.home_autoswiper', {
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction:false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+  },
+});
